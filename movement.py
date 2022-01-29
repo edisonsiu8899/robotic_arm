8be89b9038480cc42ec0190ae70f938daf73bce1
Edison <evsiu@usc.edu>
2022-01-28 19:33:46 -0800

    Updates

--- movement.py
+++ movement.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
 
     print("Beginning Movement")
     print(motor_param[str(1)]["direction_pin"])
+    print(speed_param["speed"]["delay"])
     io.output(motor_param[str(1)]["direction_pin"], True)
     time.sleep(speed_param["speed"]["delay"])
     io.output(motor_param[str(1)]["step_pin"], True)
