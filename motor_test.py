5033bf76088cd2fdc0294c3ba9e8b3f7a901eb66
Edison <evsiu@usc.edu>
2022-01-28 17:43:58 -0800

    Updates

--- motor_test.py
+++ motor_test.py
@@ -7,23 +7,55 @@ import sys, tty, termios, time
 #motor_enable_pin = 17
 #motor_direction_pin = 27
 #motor_step_pin = 22
-motor_enable_pin = 2
-motor_direction_pin = 3
-motor_step_pin = 4
+motor_enable_pin_1 = 2
+motor_direction_pin_1 = 3
+motor_step_pin_1 = 4
 
 motor_enable_pin_2 = 14
 motor_direction_pin_2 = 15
 motor_step_pin_2 = 18
 
+motor_enable_pin_3 = 17
+motor_direction_pin_3 = 27
+motor_step_pin_3 = 22
+
+motor_enable_pin_4 = 23
+motor_direction_pin_4 = 24
+motor_step_pin_4 = 25
+
+motor_enable_pin_5 = 10
+motor_direction_pin_5 = 9
+motor_step_pin_5 = 11
+
+motor_enable_pin_6 = 8
+motor_direction_pin_6 = 7
+motor_step_pin_6 = 1
+
 delay = 3E-004              # By playing with this delay you can influence the rotational speed.
 pulses_per_rev = 400        # This can be configured on the driver using the DIP-switches
-io.setup(motor_enable_pin, io.OUT)
-io.setup(motor_direction_pin, io.OUT)
-io.setup(motor_step_pin, io.OUT)
+io.setup(motor_enable_pin_1, io.OUT)
+io.setup(motor_direction_pin_1, io.OUT)
+io.setup(motor_step_pin_1, io.OUT)
 
 io.setup(motor_enable_pin_2, io.OUT)
 io.setup(motor_direction_pin_2, io.OUT)
 io.setup(motor_step_pin_2, io.OUT)
+
+io.setup(motor_enable_pin_3, io.OUT)
+io.setup(motor_direction_pin_3, io.OUT)
+io.setup(motor_step_pin_3, io.OUT)
+
+io.setup(motor_enable_pin_4, io.OUT)
+io.setup(motor_direction_pin_4, io.OUT)
+io.setup(motor_step_pin_4, io.OUT)
+
+io.setup(motor_enable_pin_5, io.OUT)
+io.setup(motor_direction_pin_5, io.OUT)
+io.setup(motor_step_pin_5, io.OUT)
+
+io.setup(motor_enable_pin_6, io.OUT)
+io.setup(motor_direction_pin_6, io.OUT)
+io.setup(motor_step_pin_6, io.OUT)
 
 
 # The getch method can determine which key has been pressed
@@ -50,40 +82,72 @@ def getch():
 #   Pulse low-level width not less than 2.5 micro-s.
 
 def stepper_enable():
-    io.output(motor_enable_pin, False)
+    io.output(motor_enable_pin_1, False)
     io.output(motor_enable_pin_2, False)
+    io.output(motor_enable_pin_3, False)
+    io.output(motor_enable_pin_4, False)
+    io.output(motor_enable_pin_5, False)
+    io.output(motor_enable_pin_6, False)
 
 def stepper_disable():
-    io.output(motor_enable_pin, True)
+    io.output(motor_enable_pin_1, True)
     io.output(motor_enable_pin_2, True)
+    io.output(motor_enable_pin_3, True)
+    io.output(motor_enable_pin_4, True)
+    io.output(motor_enable_pin_5, True)
+    io.output(motor_enable_pin_6, True)
 
 def step_once():
-    io.output(motor_step_pin, True)
+    io.output(motor_step_pin_1, True)
     io.output(motor_step_pin_2, True)
+    io.output(motor_step_pin_3, True)
+    io.output(motor_step_pin_4, True)
+    io.output(motor_step_pin_5, True)
+    io.output(motor_step_pin_6, True)
     time.sleep(delay)
-    io.output(motor_step_pin, False)
+    io.output(motor_step_pin_1, False)
     io.output(motor_step_pin_2, False)
+    io.output(motor_step_pin_3, False)
+    io.output(motor_step_pin_4, False)
+    io.output(motor_step_pin_5, False)
+    io.output(motor_step_pin_6, False)
     time.sleep(delay)
 
 def step_forward():
-    io.output(motor_direction_pin, True)
+    io.output(motor_direction_pin_1, True)
     io.output(motor_direction_pin_2, True)
+    io.output(motor_direction_pin_3, True)
+    io.output(motor_direction_pin_4, True)
+    io.output(motor_direction_pin_5, True)
+    io.output(motor_direction_pin_6, True)
     time.sleep(delay)
     step_once()
     
 
 def step_reverse():
-    io.output(motor_direction_pin, False)
+    io.output(motor_direction_pin_1, False)
     io.output(motor_direction_pin_2, False)
+    io.output(motor_direction_pin_3, False)
+    io.output(motor_direction_pin_4, False)
+    io.output(motor_direction_pin_5, False)
+    io.output(motor_direction_pin_6, False)
     time.sleep(delay)
     step_once()
 
 # Setting the stepper pins to false so the motors will not move
 # until the user presses the first key
-io.output(motor_enable_pin, False)
-io.output(motor_step_pin, False)
+io.output(motor_enable_pin_1, False)
+io.output(motor_step_pin_1, False)
 io.output(motor_enable_pin_2, False)
 io.output(motor_step_pin_2, False)
+io.output(motor_enable_pin_3, False)
+io.output(motor_step_pin_3, False)
+io.output(motor_enable_pin_4, False)
+io.output(motor_step_pin_4, False)
+io.output(motor_enable_pin_5, False)
+io.output(motor_step_pin_5, False)
+io.output(motor_enable_pin_6, False)
+io.output(motor_step_pin_6, False)
 
 
 # Print instructions for when the user has an interface
